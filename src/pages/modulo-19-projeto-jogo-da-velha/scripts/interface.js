6240c96bb92c3730b7112f29162d87407126f930
Matheus Rodrigues <matheus.rcg.0@gmail.com>
2025-03-19 10:20:11 -0400

    adiciona a lógica do jogo

--- src/pages/modulo-19-projeto-jogo-da-velha/scripts/interface.js
+++ src/pages/modulo-19-projeto-jogo-da-velha/scripts/interface.js
@@ -13,19 +13,19 @@ function handleClick(event) {
   let square = event.target;
   let position = square.id;
 
-  handleMove(position);
-  updateSquares();
+  if (handleMove(position)) {
+    
+    setTimeout (() => {
+      alert('O jogo acabou - O vencedor foi o jogador ' + symbols[playerTime]);
+    }, 10);
+  }
+  updateSquare(position);
 }
 
-function updateSquares() {
-  let squares = document.querySelectorAll('.square');
 
-  squares.forEach((square) => {
-    let position = square.id;
-    let symbol = board[position];
 
-    if (symbol != '') {
-      square.innerHTML = `<div class='${symbol}'></div>`
-    }
-  })
+function updateSquare() {
+  let square = document.getElementById(position.toString());
+  let symbol = board[position];
+  square.innerHTML = `<div class='${symbol}'></div>`;
 }
