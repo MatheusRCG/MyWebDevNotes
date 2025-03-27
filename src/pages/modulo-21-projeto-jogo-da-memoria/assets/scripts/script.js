af542cabdf174b20e2541f5bf9204d1c454bb90b
Matheus Rodrigues <matheus.rcg.0@gmail.com>
2025-03-27 10:57:37 -0400

    feat/inicializa o tabuleiro as  cartas no jogo da memória

--- src/pages/modulo-21-projeto-jogo-da-memoria/assets/scripts/script.js
+++ src/pages/modulo-21-projeto-jogo-da-memoria/assets/scripts/script.js
@@ -1,3 +1,8 @@
+const FRONT = "card-front";
+const BACK = "card-back";
+const CARD = "card";
+const ICON = "icon";
+
 let techs = [
   "bootstrap",
   "css",
@@ -12,15 +17,15 @@ let techs = [
 ];
 
 function startGame() {
-  cards = createCards(techs);
+  let cards = createCards(techs);
   shuffleCards(cards);
-  console.log(cards);
+  initializeBoard(cards);
 }
 
 function createCards(techs) {
   let cards = [];
 
-  cards.forEach((tech) => {
+  techs.forEach((tech) => {
     cards.push(createPair(tech));
   });
 
@@ -59,5 +64,50 @@ function shuffleCards(cards) {
     ];
   }
 }
+
+/*
+  inicializar tabuleiro
+    criar cada card dentro de tabuleiro
+  criar a carta front e back
+  criar o evento de click
+*/
+
+function initializeBoard(cards) {
+  let board = document.getElementById("game-board");
+
+  cards.forEach((card) => {
+    let cardElement = document.createElement("div");
+    cardElement.id = card.id;
+    cardElement.classList.add(CARD);
+    cardElement.dataset.icon = card.icon;
+
+    createCardContent(card, cardElement);
+
+    cardElement.addEventListener("click", flipCard);
+    board.appendChild(cardElement);
+  });
+}
+
+function createCardContent(card, cardElement) {
+  createCardFace(FRONT, card, cardElement);
+  createCardFace(BACK, card, cardElement);
+}
+
+function createCardFace(face, card, cardElement) {
+  let cardElementFace = document.createElement("div");
+  cardElementFace.classList.add(face);
+  if (face === FRONT) {
+    let iconElement = document.createElement("img");
+    iconElement.classList.add(ICON);
+    iconElement.src = "./assets/images/" + card.icon + ".png";
+    iconElement.alt = card.icon;
+    cardElementFace.appendChild(iconElement);
+  } else {
+    cardElementFace.innerHTML = "&lt;/&gt;";
+  }
+  cardElement.appendChild(cardElementFace);
+}
+
+function flipCard() {}
 
 startGame();
